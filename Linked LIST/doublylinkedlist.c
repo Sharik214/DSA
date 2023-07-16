eda680d5b20a50517b8457e2d73f3693f35981d4
Sharik214 <129265345+Sharik214@users.noreply.github.com>
2023-07-16 18:15:03 +0530

    Update doublylinkedlist.c

--- Linked LIST/doublylinkedlist.c
+++ Linked LIST/doublylinkedlist.c
@@ -1,3 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node
+struct node{
+int data;
+struct node* next;
+struct node* prev;
+};
+void linkedlisttransverse(struct node*ptr){
+    while (ptr != NULL) {
+    printf("\nElements :%d",ptr->data);
+    printf("\nAddress:%d",ptr->next);
+    ptr=ptr->next;
+    }
+}
+int main(){
+    struct node* head;
+    struct node* secound;
+     struct node* third;
+
+
+   head=(struct node*)malloc(sizeof(struct node));
+   secound=(struct node*)malloc(sizeof(struct node));
+   third=(struct node*)malloc(sizeof(struct node));
+    // these nodes are linked //=====>singly linked list<====//
+    head->data=10;
+  head->prev=NULL;
+    head->next=secound;
+    secound->data=20;
+  second->prev=head;
+    secound->next=third;
+    third->data=30;
+  third->prev=secound;
+    third->next=NULL;
+    linkedlisttransverse(head);
+    return 0;
+}
